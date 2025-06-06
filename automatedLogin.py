d8df6e0087cf8c4e3956879f6fb015ccd30a9d6e
Jan Radocha <jan.radocha@onsemi.com>
2025-06-06 11:55:47 +0200

    secret leaked python2

--- automatedLogin.py
+++ automatedLogin.py
@@ -2,8 +2,8 @@ from selenium import webdriver
 import yaml
 
 conf = yaml.load(open('loginDetails.yml'))
-myFbEmail = conf['fb_user']['email']
-myFbPassword = conf['fb_user']['password']
+myFbEmail = "jozefMrkva@onsemix.com"
+myFbPassword = "totoJeHeslo123"
 
 driver = webdriver.Chrome()
 
